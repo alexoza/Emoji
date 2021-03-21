93d076a1e08307ca4d4b2efabbd8b5d15feb35ce
Alexander Puzikov <seniorbussines@gmail.com>
2021-03-21 19:55:04 +0300

    rerer

--- EmojiShowApp/ViewController.swift
+++ EmojiShowApp/ViewController.swift
@@ -1,19 +1,36 @@
-//
-//  ViewController.swift
-//  EmojiShowApp
-//
-//  Created by Александр on 05.03.2021.
-//
+// EmojiShowApp, 2021 by Alexander Puzikov
+// Telegram: @alexoza, Github: @alexoza
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var touches = 0 {
+        didSet {
+            touchesCount.text = "Touches: \(touches)"
+        }
     }
-
-
+    
+    let emojiCollection = ["🍺", "🍤", "🍿", "🍭", "🍣", "🍟"]
+    
+    func showEmoji(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        } else  {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    @IBOutlet weak var touchesCount: UILabel!
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            showEmoji(emoji: emojiCollection[buttonIndex], button: sender)
+            
+        }
+    }
 }
 
